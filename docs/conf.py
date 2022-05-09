f90ca1b5439555e3afa1d3a2c89fb796c6525352
jasquat <jasquat@users.noreply.github.com>
2022-05-09 16:11:19 -0400

    first commit

---
+++ docs/conf.py
@@ -0,0 +1,14 @@
+"""Sphinx configuration."""
+from datetime import datetime
+
+
+project = "Flask Bpmn"
+author = "Jason Lantz"
+copyright = f"{datetime.now().year}, {author}"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_click",
+]
+autodoc_typehints = "description"
+html_theme = "furo"
---
+++ src/flask_bpmn/__main__.py
@@ -0,0 +1,12 @@
+"""Command-line interface."""
+import click
+
+
+@click.command()
+@click.version_option()
+def main() -> None:
+    """Flask Bpmn."""
+
+
+if __name__ == "__main__":
+    main(prog_name="flask-bpmn")  # pragma: no cover
