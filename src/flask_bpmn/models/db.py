ff66b71bbe0a19a1f5bde06779b803c328d02766
burnettk <burnettk@users.noreply.github.com>
2022-06-22 21:50:33 -0400

    pre-commit

--- src/flask_bpmn/models/db.py
+++ src/flask_bpmn/models/db.py
@@ -1,13 +1,13 @@
 """Db."""
 from __future__ import annotations
-from typing import Type
+
+import time
 
 from flask_migrate import Migrate  # type: ignore
 from flask_sqlalchemy import SQLAlchemy  # type: ignore
 from sqlalchemy import event  # type: ignore
-from sqlalchemy.orm.mapper import Mapper  # type: ignore
 from sqlalchemy.engine.base import Connection  # type: ignore
-import time
+from sqlalchemy.orm.mapper import Mapper  # type: ignore
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -15,10 +15,11 @@ migrate = Migrate()
 
 class SpiffworkflowBaseDBModel(db.Model):  # type: ignore
     """SpiffworkflowBaseDBModel."""
+
     __abstract__ = True
 
     @classmethod
-    def _all_subclasses(cls) -> list[Type[SpiffworkflowBaseDBModel]]:
+    def _all_subclasses(cls) -> list[type[SpiffworkflowBaseDBModel]]:
         """Get all subclasses of cls, descending.
 
         So, if A is a subclass of B is a subclass of cls, this
@@ -36,7 +37,9 @@ class SpiffworkflowBaseDBModel(db.Model):  # type: ignore
         return result
 
 
-def update_created_modified_on_create_listener(mapper: Mapper, _connection: Connection, target: SpiffworkflowBaseDBModel) -> None:
+def update_created_modified_on_create_listener(
+    mapper: Mapper, _connection: Connection, target: SpiffworkflowBaseDBModel
+) -> None:
     """Event listener that runs before a record is updated, and sets the create/modified field accordingly."""
     if "created_at_in_seconds" in mapper.columns.keys():
         target.created_at_in_seconds = round(time.time())
@@ -44,7 +47,9 @@ def update_created_modified_on_create_listener(mapper: Mapper, _connection: Conn
         target.updated_at_in_seconds = round(time.time())
 
 
-def update_modified_on_update_listener(mapper: Mapper, _connection: Connection, target: SpiffworkflowBaseDBModel) -> None:
+def update_modified_on_update_listener(
+    mapper: Mapper, _connection: Connection, target: SpiffworkflowBaseDBModel
+) -> None:
     """Event listener that runs before a record is updated, and sets the modified field accordingly."""
     if "updated_at_in_seconds" in mapper.columns.keys():
         if db.session.is_modified(target, include_collections=False):
@@ -57,5 +62,5 @@ def add_listeners() -> None:
     This should be called after importing all subclasses
     """
     for cls in SpiffworkflowBaseDBModel._all_subclasses():
-        event.listen(cls, 'before_insert', update_created_modified_on_create_listener)
-        event.listen(cls, 'before_update', update_modified_on_update_listener)
+        event.listen(cls, "before_insert", update_created_modified_on_create_listener)
+        event.listen(cls, "before_update", update_modified_on_update_listener)
