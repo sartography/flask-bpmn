b2d0c8c0776ebb5cd25adec3a9a22089ae70a41d
burnettk <burnettk@users.noreply.github.com>
2022-07-01 13:45:27 -0400

    removed unused expression

--- tests/flask_bpmn/models/test_group.py
+++ tests/flask_bpmn/models/test_group.py
@@ -14,7 +14,6 @@ def test_table_names_are_singular_per_what_appear_to_be_flask_conventions() -> N
 
 def test__all_subclasses_of_spiffworkflow_base_db_model_returns_all_subclasses_that_are_defined() -> None:
     """Test_is_jsonable_can_succeed."""
-    FlaskBpmnGroupModel
     assert SpiffworkflowBaseDBModel._all_subclasses() == [
         FlaskBpmnGroupModel,
         AppGroupModel,
